e0c29eddb5d7127e7fdd50a110021c22eab0001f
Sebastian <anguielugo44@gmail.com>
2021-08-25 13:37:47 -0500

    Cambios nuevos

--- prueba3.py
+++ prueba3.py
@@ -1,1 +1,6 @@
-print("El mundo que grita en el silencio")
+numero1 = int(input("Ingresa primer Numero--> "))
+numero2 = int(input("Ingresa primer Numero--> "))
+
+resultado = numero1 + numero2
+
+print(f"El mundo que grita en el silencio --> {resultado}")
